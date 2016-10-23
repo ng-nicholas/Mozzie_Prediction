f537188508f6d289bbd1d40cec82e8f2ce0f1ac7
Nicholas Ng <ng.nicholas@outlook.com>
2016-10-23 13:57:24 +0800

    Added descriptions and comments to station and dailyweather

--- Station_Cleaning.R
+++ Station_Cleaning.R
@@ -1,11 +1,28 @@
+################################################################################
+# Cleaning of list of SMS weather stations
+# Last updated: 2016-10-23
+#
+# Because the Singapore Meteorological service stores information on their
+# weather stations in a pdf document, this script extracts a list of details of
+# each station, including: station names and lat/long coordinates.
+################################################################################
+
 library(pdftools)
 
-text <- pdf_text(readline())
+# Defining folder
+fldr <- readline("Path of folder: ")
 
+# Reading in only the text from the document
+text <- pdf_text(file.path(fldr, readline("Filename of pdf doc: ")))
+
+# Splitting each line by carriage-return text
 textpg1 <- unlist(strsplit(text[1], "\\r\\n"))
 textpg2 <- unlist(strsplit(text[2], "\\r\\n"))
 textpg3 <- unlist(strsplit(text[3], "\\r\\n"))
+
+# Combining all character vectors minus the repeated table headers
 textcomb <- c(textpg1, textpg2[5:35], textpg3[5:13])
 # textcombsplit <- strsplit(textcomb, "\\s{2,}")
 
-write.csv(textcomb, readline())
+# Writing the character vector into a csv file.
+write.csv(textcomb, file.path(fldr, readline("Filename of converted csv: ")))
--- dailyweather_download.R
+++ dailyweather_download.R
@@ -1,5 +1,30 @@
+################################################################################
+# Scraping and combination of SMS daily weather data
+# Last updated: 2016-10-23
+#
+# The Singapore Meteorological Service stores daily weather data captured in the
+# past as csv files on their website at
+# http://www.weather.gov.sg/climate-historical-daily/ . Unfortunately, they
+# use javascript to make users pull data for each station by month, making it
+# difficult for humans to pull data for a long date period. Fortunately, the
+# data is stored in csv files which are accessible via urls. This script aims to
+# download all of the data for a specified period and combine it into one table.
+################################################################################
+
 library(curl)
 library(lubridate)
+library(data.table)
 
-dateseq <- seq.Date(ymd("2015-01-01"), ymd(Sys.Date()), by = "month")
+fldr <- readline("Path of folder: ")
 
+# Creating a sequence of months given 2 dates
+dateseq <- seq.Date(
+    from = ymd("2015-01-01"),
+    to = ymd(Sys.Date()),
+    by = "month"
+)
+
+# Reading in station data
+stations <- fread(
+
+)
