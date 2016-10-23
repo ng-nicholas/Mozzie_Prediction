be45a22519bf148af50015f7589e8d21c0775658
Nicholas Ng <ng.nicholas@outlook.com>
2016-10-23 17:14:03 +0800

    Completion of dailyweather download script as well as making the station list cleaning script more interactive.

--- Station_Cleaning.R
+++ Station_Cleaning.R
@@ -9,11 +9,8 @@
 
 library(pdftools)
 
-# Defining folder
-fldr <- readline("Path of folder: ")
-
 # Reading in only the text from the document
-text <- pdf_text(file.path(fldr, readline("Filename of pdf doc: ")))
+text <- pdf_text(readline("Path & filename of pdf doc: "))
 
 # Splitting each line by carriage-return text
 textpg1 <- unlist(strsplit(text[1], "\\r\\n"))
@@ -25,4 +22,4 @@ textcomb <- c(textpg1, textpg2[5:35], textpg3[5:13])
 # textcombsplit <- strsplit(textcomb, "\\s{2,}")
 
 # Writing the character vector into a csv file.
-write.csv(textcomb, file.path(fldr, readline("Filename of converted csv: ")))
+write.csv(textcomb, readline("Path & filename of converted csv: "))
--- dailyweather_download.R
+++ dailyweather_download.R
@@ -11,20 +11,84 @@
 # download all of the data for a specified period and combine it into one table.
 ################################################################################
 
-library(curl)
+# library(curl)
 library(lubridate)
 library(data.table)
 
-fldr <- readline("Path of folder: ")
-
 # Creating a sequence of months given 2 dates
 dateseq <- seq.Date(
-    from = ymd("2015-01-01"),
-    to = ymd(Sys.Date()),
+    from = ymd(readline("From date (YYYY-MM-DD): ")),
+    to = ymd(readline("To date (YYYY-MM-DD): ")),
     by = "month"
 )
 
 # Reading in station data
 stations <- fread(
-
+    readline("Path & filename of station list: "),
+    check.names = T
 )
+stations <- stations[Station_Code != ""]
+
+# Creating a table of filenames
+station_tbl <- NULL
+for (i in 1:length(dateseq)) {
+    stations_copy <- copy(stations)
+
+    stations_copy[, `:=`(
+        Filename = paste0(
+            "DAILYDATA_",
+            Station_Code,
+            "_",
+            format(dateseq[i], "%Y%m"),
+            ".csv"
+        )
+    )]
+
+    station_tbl <- rbindlist(list(station_tbl, stations_copy))
+}
+rm(stations, stations_copy)
+
+# Scraping data from SMS website files
+weather_data <- NULL
+for (i in 1:nrow(station_tbl)) {
+    try({
+        ## Downloading the data and reading in as data table
+        point_data <- fread(
+            paste0("http://www.weather.gov.sg/files/dailydata/",
+                   station_tbl$Filename[i])
+        )
+
+        ## Cleaning the headers
+        rev_headers <- gsub("[[:punct:]]", "", names(point_data))
+        rev_headers <- gsub("\\s", "_", rev_headers)
+
+        setnames(
+            point_data,
+            rev_headers
+        )
+
+        ## Adding station code for easier merging later
+        point_data[, Station_Code := station_tbl$Station_Code[i]]
+
+        ## Inserting rows
+        weather_data <- rbindlist(list(weather_data, point_data))
+    }, silent = T)
+}
+rm(point_data)
+
+# Combine weather data with station table
+weather_data <- merge(
+    weather_data,
+    unique(station_tbl[, c("Lat", "Long", "Station_Code"), with = F]),
+    by = "Station_Code"
+)
+
+# Write data out to csv file
+write.csv(
+    weather_data,
+    readline("Path & filename of output csv: "),
+    row.names = F
+)
+
+# Clear all variables
+rm(list = ls())
